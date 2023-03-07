57dbaa01749397f8afda33ffae81bc9a9c89dafc
Izabela França <101933646+izabela-franca@users.noreply.github.com>
2023-03-07 09:08:45 -0300

    Update multiplos.java

--- teste-pratica-evoluservices/ex1_ticoTeco/multiplos.java
+++ teste-pratica-evoluservices/ex1_ticoTeco/multiplos.java
@@ -1,5 +1,15 @@
 package ex1_ticoTeco;
 
+/*
+1- Escreva um programa que imprima os números de 1 a 100. Mas para múltiplos de três imprima 'Tico' em vez do número e para os múltiplos de cinco imprima 'Teco'.
+Para números que são múltiplos de três e cinco imprimir 'TicoTeco'.
+Exemplo, para o caso de 1 a 5, o programa nos daria '1, 2, Tico, 4, Teco'.
+Utilize o esqueleto a seguir para implementar seu código:
+
+public void ticoTeco() {// Seu código}
+
+*/
+
 public class multiplos {
 
     public void ticoTeco() {
